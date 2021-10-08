2b5011d42a0c0e058201808ef99a69b75e731686
daiguimei <2710413219@qq.com>
2021-10-08 17:31:46 +0800

    hot-fix commit1

--- src/main/java/com/example/helloword/HelloController.java
+++ src/main/java/com/example/helloword/HelloController.java
@@ -3,5 +3,6 @@ package com.example.helloword;
 public class HelloController {
     public static void main(String[]args){
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
